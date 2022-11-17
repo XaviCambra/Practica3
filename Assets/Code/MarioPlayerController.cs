45bc920b312baa51c7f602ade41867440f41db33
Sora <bmartinezf@edu.tecnocampus.cat>
2022-11-17 17:30:48 +0100

    cambio en mario controller

--- Assets/Code/MarioPlayerController.cs
+++ Assets/Code/MarioPlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MarioPlayerController : MonoBehaviour
 {
+    public CameraController m_Camera;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,29 @@ public class MarioPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 l_ForwardCamera = m_Camera.transform.forward;
+        Vector3 l_RightCamera = m_Camera.transform.right;
+        l_ForwardCamera.y = 0.0f;
+        l_RightCamera.y = 0.0f;
+        l_ForwardCamera.Normalize();
+        l_RightCamera.Normalize();
 
+        Vector3 l_Movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            l_Movement = l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            l_Movement = -l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            l_Movement -= l_RightCamera;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            l_Movement += l_RightCamera;
+        }
     }
 }
