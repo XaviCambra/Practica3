dad4ab272caddf3826a593206d13e6ee75c291e6
Sora <bmartinezf@edu.tecnocampus.cat>
2022-12-03 15:25:05 +0100

    cambios del viernes

--- Assets/Code/Enemies/Goomba.cs
+++ Assets/Code/Enemies/Goomba.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Goomba : MonoBehaviour, IRestartGameElement
 {
     public float m_KillTime = 0.5f;
     public float m_KillScale = 0.2f;
+    public float m_Alert = 2.0f;
+    public Transform m_Mario;
+    NavMeshPath m_AttackPath;
+    public float m_Speed = 5.0f;
+
+    public enum TStates
+    {
+        PATROL,
+        ALERT,
+        ATTACK,
+        DEAD
+    }
+
+    TStates m_CurrentState;
 
     private void Start()
     {
+        m_CurrentState = TStates.PATROL;
         GameController.GetGameController().AddRestartGameElement(this);
+        m_AttackPath = new NavMeshPath();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.J))
+            m_CurrentState = TStates.ALERT;
+        if(m_CurrentState == TStates.ALERT)
+        {
+            StartCoroutine(Asustao());
+        }
+        else if (m_CurrentState == TStates.ATTACK)
+        {
+            
+        }
     }
     public void Kill()
     {
@@ -21,6 +52,15 @@ public class Goomba : MonoBehaviour, IRestartGameElement
         yield return new WaitForSeconds(m_KillTime);
         gameObject.SetActive(false);
 
+    }
+    IEnumerator Asustao()
+    {
+        yield return new WaitForSeconds(m_Alert);
+        NavMesh.CalculatePath(transform.position, m_Mario.position, NavMesh.AllAreas, m_AttackPath);
+ 
+        for(int i = 0; i< m_AttackPath.corners.Length-1;i++)
+            Debug.DrawLine(m_AttackPath.corners[i], m_AttackPath.corners[i + 1], Color.red);
+        m_CurrentState = TStates.ATTACK;
     }
     public void RestartGame()
     {
